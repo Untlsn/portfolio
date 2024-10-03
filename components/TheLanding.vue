3bbd0af65ec9f7486ed230f744fc654081d6ddf7
Filip Skoczeń <philipskoczen@gmail.com>
2024-10-03 14:32:33 +0200

    Add media

--- components/TheLanding.vue
+++ components/TheLanding.vue
@@ -6,41 +6,77 @@ const chain = createChain(true);
 </script>
 
 <template>
-	<div class="min-h-screen mt-60">
-		<article class="flex-(~ wrap) gap-20 items-center mx-1/8">
-			<h1 class="text-16 lg:text-24 grid-(~ cols-[1fr_6rem]) w-min">
-				<span class="block"><BaseTypeingText start text="Cześć" /></span>
-				<span class="block rotate-90 w-8 h-38 -translate-y-12 row-span-2">
-					<BaseTypeingText text="Tutaj" />
-				</span>
-				<span
-					class="text-end relative block before:(i-ph-caret-right-bold absolute top-0 -left-4 blinking)"
-					:class="chain[2] && 'before:c_'"
-				>
-					<BaseTypeingText text="Filip" reverce />
-				</span>
-			</h1>
-			<ul class="list-disc text-6 lg:text-9">
-				<li class="empty:hidden">
-					<BaseTypeingText text="Web desinger" />
+	<div class="flex mx-20 mt-60 gap-12 justify-between">
+		<div class="min-h-screen">
+			<article class="flex-(~ wrap) gap-20">
+				<h1 class="text-16 lg:text-24 grid-(~ cols-[1fr_6rem]) w-min">
+					<span class="block"><BaseTypeingText start text="Cześć" /></span>
+					<span class="block rotate-90 w-8 h-38 -translate-y-12 lg:translate-x-8 row-span-2">
+						<BaseTypeingText text="Tutaj" />
+					</span>
+					<span
+						class="text-end relative block before:(i-ph-caret-right-bold absolute top-0 -left-4 blinking)"
+						:class="chain[2] && 'before:c_'"
+					>
+						<BaseTypeingText text="Filip" reverce />
+					</span>
+				</h1>
+				<ul class="list-disc text-6 lg:text-9">
+					<li class="empty:hidden">
+						<BaseTypeingText text="Web desinger" />
+					</li>
+					<li class="empty:hidden">
+						<BaseTypeingText text="Web desinger" />
+					</li>
+					<li class="empty:hidden">
+						<BaseTypeingText text="FOOS enjoyer" />
+					</li>
+					<li class="empty:hidden">
+						<BaseTypeingText text="Father" />
+					</li>
+				</ul>
+			</article>
+		</div>
+		<nav class="show-from-right">
+			<h2 class="text-8">
+				Media
+			</h2>
+			<ul class="pl-4 text-4 leading-6">
+				<li class="before:(c_ i-line-md-github)">
+					<a href="https://github.com/untlsn" target="_blank">
+						Github
+					</a>
 				</li>
-				<li class="empty:hidden">
-					<BaseTypeingText text="Web desinger" />
+				<li class="before:(c_ i-simple-icons-exercism)">
+					<a href="https://exercism.org/profiles/Untlsn" target="_blank">
+						Exercism
+					</a>
 				</li>
-				<li class="empty:hidden">
-					<BaseTypeingText text="FOOS enjoyer" />
+				<li class="before:(c_ i-simple-icons-freelancer)">
+					<a href="https://www.freelancer.com/u/Untlsn" target="_blank">
+						Freelancer
+					</a>
 				</li>
-				<li class="empty:hidden">
-					<BaseTypeingText text="Father" />
+				<li class="before:(c_ i-simple-icons-upwork)">
+					<a href="https://www.upwork.com/freelancers/~01c55b178beb0df537?viewMode=1" target="_blank">
+						Upwork
+					</a>
 				</li>
 			</ul>
-		</article>
+		</nav>
 	</div>
 </template>
 
 <style scoped>
+.show-from-right {
+	animation: show-from-right .5s forwards;
+}
+
+@keyframes show-from-right {
+	from { translate: 100vw }
+}
 .before\:blinking::before {
-	animation: blinking .7s infinite steps(2, end);
+	animation: blinking .8s 4 steps(2, end);
 	opacity: var(--opacity);
 }
 
